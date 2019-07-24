5389e212a216656ae0c9427a62d7fac9b0152fd8
Unknown <jsphillips2@wisc.edu>
2019-07-24 16:05:22 +0000

    Updated export of extracted HOBO log file

--- data/raw_data/extract_data.R
+++ data/raw_data/extract_data.R
@@ -25,6 +25,7 @@ benthic_grad <- read_excel(path, sheet = "BenthicGradient", na=c("","NA"))
 pelagic_grad <- read_excel(path, sheet = "Pelagic", na=c("","NA"))
 light_profile <- read_excel(path, sheet = "LightProfiles", na=c("","NA"))
 shading <- read_excel(path, sheet = "Shading", na=c("","NA"))
+hobo_log <- read_excel(path, sheet = "HOBOLog", na=c("","NA")) 
 
 # correct light meter readings taken out in air (-0.05m) prior to 2019
 # in-water calibration multiplier for UW-LTREB Li-Cor meter is -344.88
@@ -45,6 +46,7 @@ light_profile <- light_profile %>%
 # write_csv(pelagic_grad, "data/raw_data/extracted/pelagic_grad.csv")
 # write_csv(light_profile, "data/raw_data/extracted/light_profile.csv")
 # write_csv(shading, "data/raw_data/extracted/shading.csv")
+# write_csv(hobo_log, "data/raw_data/extracted/hobo_log.csv")
 
 
 
