bc403a217a2a1410219d72adc8ddfa704dabadf4
Unknown <jsphillips2@wisc.edu>
2019-07-24 13:20:37 +0000

    Process HOBO data for E5 (to update)

--- data/clean_data/clean_data.R
+++ data/clean_data/clean_data.R
@@ -7,39 +7,27 @@ library(tidyverse)
 library(lubridate)
 
 # import data
-benthic = read_csv("data/raw_data/benthic_grad.csv") 
-pelagic = read_csv("data/raw_data/pelagic_grad.csv") 
-profiles = read_csv("data/raw_data/light_profile.csv") 
-shading = read_csv("data/raw_data/shading.csv") 
-
-# define base theme
-theme_base = theme_bw()+
-  theme(panel.grid = element_blank(),
-        strip.background = element_blank(),
-        legend.margin = margin(0,0,0,0),
-        text = element_text(size=12),
-        strip.text = element_text(size=10),
-        legend.text = element_text(size=10),
-        axis.text=element_text(size=10, color="black"),
-        axis.title.y=element_text(margin=margin(0,15,0,0)),
-        axis.title.x=element_text(margin=margin(15,0,0,0)))
+benthic <- read_csv("data/raw_data/extracted/benthic_grad.csv") 
+pelagic <- read_csv("data/raw_data/extracted/pelagic_grad.csv") 
+profiles <- read_csv("data/raw_data/extracted/light_profile.csv") 
+shading <- read_csv("data/raw_data/extracted/shading.csv") 
 
 
 
 
 
 #==========
-#========== Process data: Light
+#========== Process data: Light (from Li-Cor meter)
 #==========
 
 # calculate effect of shading
-shading_trt  = shading %>%
+shading_trt <- shading %>%
   mutate(light_frac = ifelse(par_in > par_out, 1, par_in/par_out)) %>%
   group_by(light_trt) %>%
   summarize(light_frac = mean(light_frac))
 
 # benthic light
-benthic_light = benthic %>%
+benthic_light <- benthic %>%
   group_by(sampledate, site) %>%
   summarize(time_start = min(time_start),
             time_end = max(time_end),
@@ -54,7 +42,7 @@ benthic_light = benthic %>%
   summarize(par = mean(par))
 
 # pelagic light
-pelagic_light = pelagic %>%
+pelagic_light <- pelagic %>%
   filter(is.na(time_start)==F, is.na(time_end)==F, site != "grim") %>%
   group_by(sampledate, site) %>%
   summarize(time_start = min(time_start),
@@ -70,13 +58,13 @@ pelagic_light = pelagic %>%
   summarize(par = mean(par))
 
 # add light data to benthic
-benthic_full = benthic %>%
+benthic_full <- benthic %>%
   left_join(benthic_light) %>%
   left_join(shading_trt) %>%
   mutate(par = light_frac*par)
 
 # add light data to benthic
-pelagic_full = pelagic %>%
+pelagic_full <- pelagic %>%
   filter(site != "grim") %>%
   left_join(pelagic_light) %>%
   left_join(shading_trt) %>%
@@ -87,11 +75,101 @@ pelagic_full = pelagic %>%
 
 
 #==========
+#========== Process data: Light (HOBO)
+#==========
+
+# import all files
+hobo_names <- list.files("data/raw_data/hobo")
+hobo <- hobo_names %>%
+  lapply(function(x){read_csv(paste0("data/raw_data/hobo/", x), skip = 1) %>%
+      mutate(site = str_split(x,"_")[[1]][1],
+             loc = str_split(x,"_")[[1]][2])})
+hobo[[1]]
+
+# rename and select relevant columns
+hobo_slim <- hobo %>%
+  lapply(function(x){
+    x %>%
+      rename(date_time = grep("Date",names(hobo[[1]])),
+             lux = grep("Intensity",names(hobo[[1]]))) %>%
+      select(site, loc, date_time, lux) %>%
+      return()
+  }) %>%
+  bind_rows()
+
+# convert lux to par using standard correction for sunlight (divide by 54)
+# approximate external par as par at 0.5cm (multiply by 0.44; based on analysis in sonde_oxygen)
+hobo_clean = hobo_slim %>%
+  mutate(par = lux/54,
+         par = ifelse(loc == "out", 0.44*par, par))
+
+# e5 benthic
+e5_benthic <- benthic_full %>%
+  filter(site == "e5") %>%
+  group_by(sampledate, site) %>%
+  summarize(time_start = min(time_start),
+            time_end = max(time_end),
+            depth = unique(inc_depth)) %>%
+  left_join(hobo_clean %>%
+              filter(loc == "out")) %>%
+  mutate(within = date_time > time_start - 60*30 & date_time < time_end + 60*30) %>%
+  filter(within == T) %>%
+  rename(par0 = par) %>%
+  select(-time_start, -time_end, -lux, -within, -loc) %>%
+  left_join(hobo_clean %>%
+              filter(loc == "bottom") %>%
+              rename(parX = par) %>%
+              select(-lux, - loc)) %>%
+  mutate(atten = -log(parX/par0)/2.5,
+         par = par0*exp(-mean(atten, na.rm=T)*0.5)) %>%
+  group_by(sampledate, site) %>%
+  summarize(par = mean(par))
+
+# e5 pelagic
+e5_pelagic <- pelagic_full %>%
+  filter(site == "e5") %>%
+  group_by(sampledate, site) %>%
+  summarize(time_start = min(time_start),
+            time_end = max(time_end),
+            depth = unique(inc_depth)) %>%
+  left_join(hobo_clean %>%
+              filter(loc == "out")) %>%
+  mutate(within = date_time > time_start - 60*30 & date_time < time_end + 60*30) %>%
+  filter(within == T) %>%
+  rename(par0 = par) %>%
+  select(-time_start, -time_end, -lux, -within, -loc) %>%
+  left_join(hobo_clean %>%
+              filter(loc == "bottom") %>%
+              rename(parX = par) %>%
+              select(-lux, - loc)) %>%
+  mutate(atten = -log(parX/par0)/2.5,
+         par = par0*exp(-mean(atten, na.rm=T)*0.5)) %>%
+  group_by(sampledate, site) %>%
+  summarize(par = mean(par))
+
+# add to full data
+benthic_full <- benthic_full %>%
+  left_join(e5_benthic %>% rename(par2 = par)) %>%
+  left_join(shading_trt) %>%
+  mutate(par2 = light_frac*par2,
+         par = ifelse(sampledate=="2019-07-22", par2, par)) %>%
+  select(-par2)
+
+pelagic_full <- pelagic_full %>%
+  left_join(e5_pelagic %>% rename(par2 = par)) %>%
+  left_join(shading_trt) %>%
+  mutate(par2 = light_frac*par2,
+         par = ifelse(sampledate=="2019-07-22", par2, par)) %>%
+  select(-par2)
+
+
+
+#==========
 #========== Process data: DO Flux
 #==========
 
 # benthic
-bethic_clean = benthic_full %>%
+benthic_clean <- benthic_full %>%
   mutate(duration = as.numeric(time_end - time_start),
          # calculate change in do
          # multiply by 1000 to convert to mg m^-3
@@ -101,10 +179,10 @@ bethic_clean = benthic_full %>%
          do_flux = 1000*(column_depth/100)*(do_end - do_start)/1000/duration,
          # mean temperature
          temp = (temp_start + temp_end)/2) %>%
-  select(id, sampledate, site, rack, par, temp,midge_tubes, do_flux)
+  select(id, sampledate, site, rack, par, temp, midge_tubes, do_flux)
 
 # pelagic
-pelagic_clean = pelagic_full %>%
+pelagic_clean <- pelagic_full %>%
   mutate(duration = as.numeric(time_end - time_start),
          # calculate change in do
          # multiply by 1000 to convert to mg m^-3
@@ -124,7 +202,7 @@ pelagic_clean = pelagic_full %>%
 #========== Export
 #==========
 
-# write_csv(bethic_clean, "data/clean_data/benthic_clean.csv")
+# write_csv(benthic_clean, "data/clean_data/benthic_clean.csv")
 # write_csv(pelagic_clean, "data/clean_data/pelagic_clean.csv")
 
 
